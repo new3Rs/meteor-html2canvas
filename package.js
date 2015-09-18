8bdb43e91014e4eba2e4168602c672a28676ee6f
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2015-09-18 22:01:01 +0900

    2015/08/30

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'new3rs:html2canvas',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'html2canvas 0.5.0-alpha2',
   // URL to the Git repository containing the source code for this package.
