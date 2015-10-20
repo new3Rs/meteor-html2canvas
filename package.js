55bf0f7ae463b02a848837acb434491da525123b
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2015-10-20 11:28:59 +0900

    0.5.0-beta2

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'new3rs:html2canvas',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
-  summary: 'html2canvas 0.5.0-alpha2',
+  summary: 'html2canvas 0.5.0-beta2',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
